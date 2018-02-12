958f8d223dae243f63b2e92575dde0ad39bc3cf5
DESKTOP-TT6ML10\ascha <ascharapon.s@gmail.com>
2018-02-12 22:34:22 +0700

    no message

--- App.js
+++ App.js
@@ -1,58 +1,20 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import { Text } from 'native-base'
+//import { Router, Scene } from 'react-native-router-flux'
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import OrderScreen from './src/screens/order/Order';
+import HistoryScreen from './src/screens/history/History';
+//import History from './src/History'
+//import Store from './src/Store'
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
+      StackNavigator({
+        Order: { screen: OrderScreen, initialRouteName: OrderScreen },
+        History: { screen: HistoryScreen },
+      })
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
